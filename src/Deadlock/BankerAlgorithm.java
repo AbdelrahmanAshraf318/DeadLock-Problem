74280cbdf59c7f4b34897b803454525297d7c24e
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.com>
2021-04-21 23:26:56 +0200

    Change arrayLists in BankerAlgorithm class to arrays & add new functions

--- src/Deadlock/BankerAlgorithm.java
+++ src/Deadlock/BankerAlgorithm.java
@@ -4,11 +4,54 @@ import java.util.ArrayList;
 
 public class BankerAlgorithm implements Strategies{
 
-	private ArrayList<Integer> availableInstance = new ArrayList<>();
-	private ArrayList<ArrayList<Integer>> maximumDemand;
-	private ArrayList<ArrayList<Integer>> allocatedInstance;
-	private ArrayList<ArrayList<Integer>> remainingNeed;
+	private ArrayList<Integer> availableInstance;
+	private Integer[][] maximumDemand;
+	private Integer[][] allocatedInstance;
+	private Integer[][] remainingNeed;
 	
+	private int numOfProcesses;
+	private int numOfResources;
+	
+	
+	
+
+	public BankerAlgorithm(int numOfProcesses , int numOfResources) {
+		super();
+		this.availableInstance = new ArrayList<>(this.numOfResources);
+		this.numOfProcesses = numOfProcesses;
+		this.numOfResources = numOfResources;
+		maximumDemand = new Integer[this.numOfProcesses][this.numOfResources];
+		allocatedInstance = new Integer[this.numOfProcesses][this.numOfResources];
+		remainingNeed = new Integer[this.numOfProcesses][this.numOfResources];
+	}
+
+
+	public void addInstanceForSpecificResource(Integer instance) {
+		availableInstance.add(instance);
+	}
+	
+	public void addMaxDemand(Integer maxDemand , int whichRow , int whichCol)
+	{
+		this.maximumDemand[whichRow][whichCol] = maxDemand;
+	}
+	
+	public void addAllocatedInstance(Integer allocated , int whichRow , int whichCol)
+	{
+		this.allocatedInstance[whichRow][whichCol] = allocated;
+	}
+	
+	private void calcRemainingNeed()
+	{
+		for(int i=0 ; i<numOfProcesses ; i++)
+		{
+			for(int j=0 ; j<numOfResources ; j++)
+			{
+				this.remainingNeed[i][j] = this.maximumDemand[i][j] -
+						this.allocatedInstance[i][j];
+			}
+		}
+	}
+
 	@Override
 	public void doAlgorithm() {
 		
